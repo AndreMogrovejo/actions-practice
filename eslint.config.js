edd8aed7a7d1074fa27a5a7ff06df68483cf836a
Andre Mogrovejo <andre.mogroveo@trade.ec>
2024-08-30 10:50:25 -0500

    team gamma >>> team delta

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: pluginJs.configs.recommended
+})
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('standard-with-typescript'),
+  pluginReactConfig
+]
---
+++ vitest.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line
+/// <reference types="vitest" />
+import { getViteConfig } from 'astro/config'
+
+export default getViteConfig({
+  test: {
+    silent: false
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    // globals: true,
+  }
+})
