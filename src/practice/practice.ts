8d9ac927d539103aaf78db5d669369db48d278c2
manriquedev <manriqueemanuel@gmail.com>
2024-08-30 18:13:12 -0300

    test

--- src/practice/practice.ts
+++ src/practice/practice.ts
@@ -1,6 +1,6 @@
 interface Transaction {
-    amount: number;
-    type: 'money🤑' | 'melomerezco🗿';
+  amount: number;
+  type: "money🤑" | "melomerezco🗿";
 }
 
 interface Totals {
@@ -13,13 +13,65 @@ interface Totals {
  * @param {Transaction[]} transactions - Un arreglo de objetos de transacciones.
  * @returns {Totals} - Un objeto con las propiedades totalIncome y totalExpenses.
  */
-function calculateTotals(transactions: Transaction[]): Totals {
-  //  TODO: Trabajar 😠
-  return {
-    totalIncome: 0,
-    totalExpenses: 0
-  }
-}
 
+// const calculateTotals = (transactions: Transaction[]): Totals => {
+//   let totalIncome = 0;
+//   let totalExpenses = 0;
+
+//   for (const transaction of transactions) {
+//     if (transaction.type === 'money🤑') {
+//       totalIncome += Math.abs(transaction.amount);
+//     } else if (transaction.type === 'melomerezco🗿') {
+//       totalExpenses += Math.abs(transaction.amount);
+//     }
+//   }
+
+//   return {
+//     totalIncome,
+//     totalExpenses
+//   };
+// }
+
+// const calculateTotals2 = (transactions: Transaction[]): Totals => {
+//   let totalIncome = 0;
+//   let totalExpenses = 0;
+
+//   for (const transaction of transactions) {
+//     transaction.type === 'money🤑' ? totalIncome += Math.abs(transaction.amount) :
+//       totalExpenses += Math.abs(transaction.amount);
+//   }
+
+//   return {
+//     totalIncome,
+//     totalExpenses
+//   };
+// }
+
+// const calculateTotals3 = (transactions: Transaction[]): Totals => {
+//   let totalIncome = 0;
+//   let totalExpenses = 0;
+
+//   transactions.forEach(transaction => {
+//     transaction.type === 'money🤑' ? totalIncome += Math.abs(transaction.amount) :
+//       totalExpenses += Math.abs(transaction.amount);
+//   });
+
+//   return {
+//     totalIncome,
+//     totalExpenses
+//   };
+// }
+
+const calculateTotals = (transactions: Transaction[]): Totals => {
+  return transactions.reduce(
+    (acc, { type, amount }) => (
+      type === "money🤑"
+        ? (acc.totalIncome += Math.abs(amount))
+        : (acc.totalExpenses += Math.abs(amount)),
+      acc
+    ),
+    { totalIncome: 0, totalExpenses: 0 }
+  );
+};
 
 export { calculateTotals };
